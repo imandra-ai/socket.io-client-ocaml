e97ec79488ceffd2fd25eb4113c1533283733877
Matt Bray <mattjbray@gmail.com>
2017-02-01 17:29:58 +0000

    Ping pong.

--- src/engineio_client.ml
+++ src/engineio_client.ml
@@ -216,12 +216,12 @@ module Transport = struct
             resp
             |> Response.status
             |> Code.code_of_status in
-          Lwt_io.printlf "Received status code: %i" code >>= fun () ->
+          (* Lwt_io.printlf "Received status code: %i" code >>= fun () -> *)
           if Code.is_success code then
             Cohttp_lwt_body.to_stream body
             |> Lwt_stream.map_list_s
               (fun line ->
-                 Lwt_io.printlf "Got line:          '%s'" (String.escaped line) >>= fun () ->
+                 (* Lwt_io.printlf "Got line:          '%s'" (String.escaped line) >>= fun () -> *)
                  let packets = Parser.decode_payload_as_binary line in
                  Lwt_list.iter_s log_packet packets >>= fun () ->
                  return packets)
@@ -247,33 +247,36 @@ module Transport = struct
     let write : t -> Packet.t list -> unit Lwt.t =
       fun t packets ->
         Lwt.(
-          Cohttp.(Cohttp_lwt_unix.(
-              let encoded_payload =
-                packets
-                |> List.map Parser.encode_packet
-                |> String.concat ""
-              in
-              Lwt_io.printlf "Sending POST request to '%s' with data '%s'"
-                (Uri.to_string t.uri)
-                (encoded_payload |> String.escaped)
-              >>= fun () ->
-              let body =
-                encoded_payload
-                |> Cohttp_lwt_body.of_string
-              in
-              catch
-                (fun () ->
-                   Client.post
-                     ~headers:(Header.init_with "content-type" "application/octet-stream")
-                     ~body:body
-                     t.uri >>= fun (resp, body) ->
-                   return_unit)
-                (fun exn ->
-                   match exn with
-                   | Failure "Client connection was closed" -> return_unit
-                   | exn -> fail exn
-                )
-            ))
+          match packets with
+          | [] -> return_unit
+          | _ ->
+            Cohttp.(Cohttp_lwt_unix.(
+                let encoded_payload =
+                  packets
+                  |> List.map Parser.encode_packet
+                  |> String.concat ""
+                in
+                Lwt_io.printlf "Sending POST request to '%s' with data '%s'"
+                  (Uri.to_string t.uri)
+                  (encoded_payload |> String.escaped)
+                >>= fun () ->
+                let body =
+                  encoded_payload
+                  |> Cohttp_lwt_body.of_string
+                in
+                catch
+                  (fun () ->
+                     Client.post
+                       ~headers:(Header.init_with "content-type" "application/octet-stream")
+                       ~body:body
+                       t.uri >>= fun (resp, body) ->
+                     return_unit)
+                  (fun exn ->
+                     match exn with
+                     | Failure "Client connection was closed" -> return_unit
+                     | exn -> fail exn
+                  )
+              ))
         )
 
     let open' : t -> (t * Packet.t Lwt_stream.t) Lwt.t =
@@ -320,51 +323,65 @@ module Transport = struct
 end
 
 module Socket = struct
+  type handshake =
+    { sid : string
+    ; ping_interval : int
+    ; ping_timeout : int
+    ; upgrades : string list
+    }
+
   type t =
     { ready_state : ready_state
     ; transport : Transport.t
-    ; sid : string option
+    ; handshake : handshake option
+    ; ping_sent_at : float option
+    ; pong_received_at : float option
     }
 
   let make_uri uri =
     Uri.with_query uri [("EIO", [string_of_int Parser.protocol])]
 
-  let parse_handshake packet_data =
-    match packet_data with
-    | Packet.P_None -> raise (Invalid_argument "no data")
-    | Packet.P_Binary _ -> raise (Invalid_argument "binary")
-    | Packet.P_String string ->
-      Yojson.Basic.(
-        match from_string string with
-        | `Assoc assoc ->
-          let sid =
-            match List.assoc "sid" assoc with
-            | `String sid -> sid
-            | _ -> raise (Invalid_argument "expected sid to be a string")
-          in
-          let upgrades =
-            match List.assoc "upgrades" assoc with
-            | `List ls ->
-              ls
-              |> List.map (function
-                  | `String upgrade -> upgrade
-                  | _ -> raise (Invalid_argument "expected upgrade to be a string")
-                )
-            | _ -> raise (Invalid_argument "expected upgrades to be a list")
-          in
-          let ping_interval =
-            match List.assoc "pingInterval" assoc with
-            | `Int interval -> interval
-            | _ -> raise (Invalid_argument "expected pingInterval to be an int")
-          in
-          let ping_timeout =
-            match List.assoc "pingTimeout" assoc with
-            | `Int timeout -> timeout
-            | _ -> raise (Invalid_argument "expected pingTimeout to be an int")
-          in
-          (sid, upgrades, ping_interval, ping_timeout)
-        | _ -> raise (Invalid_argument "expected an object")
-      )
+  let parse_handshake : Packet.payload -> handshake =
+    fun packet_data ->
+      match packet_data with
+      | Packet.P_None -> raise (Invalid_argument "no data")
+      | Packet.P_Binary _ -> raise (Invalid_argument "binary")
+      | Packet.P_String string ->
+        Yojson.Basic.(
+          match from_string string with
+          | `Assoc assoc ->
+            let sid =
+              match List.assoc "sid" assoc with
+              | `String sid -> sid
+              | _ -> raise (Invalid_argument "expected sid to be a string")
+            in
+            let upgrades =
+              match List.assoc "upgrades" assoc with
+              | `List ls ->
+                ls
+                |> List.map (function
+                    | `String upgrade -> upgrade
+                    | _ -> raise (Invalid_argument "expected upgrade to be a string")
+                  )
+              | _ -> raise (Invalid_argument "expected upgrades to be a list")
+            in
+            let ping_interval =
+              match List.assoc "pingInterval" assoc with
+              | `Int interval -> interval
+              | _ -> raise (Invalid_argument "expected pingInterval to be an int")
+            in
+            let ping_timeout =
+              match List.assoc "pingTimeout" assoc with
+              | `Int timeout -> timeout
+              | _ -> raise (Invalid_argument "expected pingTimeout to be an int")
+            in
+            { sid = sid
+            ; ping_interval = ping_interval
+            ; ping_timeout = ping_timeout
+            ; upgrades = upgrades
+            }
+          | _ -> raise (Invalid_argument "expected an object")
+        )
 
   let open' : Uri.t -> (t * Packet.t Lwt_stream.t) Lwt.t =
     fun uri ->
@@ -373,7 +390,9 @@ module Socket = struct
       let socket =
         { ready_state = Opening
         ; transport = transport
-        ; sid = None
+        ; handshake = None
+        ; ping_sent_at = None
+        ; pong_received_at = None
         }
       in
       Lwt.(
@@ -387,71 +406,133 @@ module Socket = struct
 
   let on_open socket packet_data =
     Lwt.(
-      let (sid, upgrades, ping_interval, ping_timeout) = parse_handshake packet_data in
-      Lwt_io.printlf "Got sid '%s'" sid >>= fun () ->
+      let handshake = parse_handshake packet_data in
+      Lwt_io.printlf "Got sid '%s'" handshake.sid >>= fun () ->
       let transport =
         Transport.Polling.{ socket.transport with
                             uri =
                               socket.transport.uri
                               |> (Util.flip Uri.remove_query_param) "sid"
-                              |> (Util.flip Uri.add_query_param) ("sid", [sid])
+                              |> (Util.flip Uri.add_query_param) ("sid", [handshake.sid])
                           }
       in
       return { socket with
-               sid = Some sid
+               handshake = Some handshake
              ; transport = transport
              }
     )
 
-  let process_packet : Packet.t -> t -> t Lwt.t =
-    fun (packet_type, packet_data) socket ->
+  let on_pong socket =
+    Lwt.(
+      return { socket with
+               pong_received_at = Some (Unix.time ())
+             }
+    )
+
+  let process_packet : t -> Packet.t -> t Lwt.t =
+    fun socket (packet_type, packet_data) ->
       Lwt.(
         match packet_type with
         | Packet.OPEN -> on_open socket packet_data
+        | Packet.PONG -> on_pong socket
         | _ -> return socket
       )
 
-  let with_connection : Uri.t -> ((unit -> Packet.t Lwt.t) -> (Packet.t -> unit Lwt.t) -> 'a Lwt.t) -> 'a Lwt.t =
+  let poll_interval = 2.0
+
+  let with_connection :  Uri.t -> ((Packet.t Lwt_stream.t) -> (Packet.t -> unit Lwt.t) -> 'a Lwt.t) -> 'a Lwt.t =
     fun uri f ->
       (* packets to send via transport *)
       let (packets_send_stream, push_packet_send) = Lwt_stream.create () in
       (* packets received over transport *)
       let (packets_recv_stream, push_packet_recv) = Lwt_stream.create () in
-      let recv () =
-        (* TODO: maybe just make the packet stream available instead of this recv function? *)
-        Lwt.(
-          Lwt_stream.get packets_recv_stream >>= function
-          | None -> Lwt.fail_with "No packets in stream"
-          | Some packet -> Lwt.return packet
-        )
-      in
       let send packet =
         push_packet_send (Some packet); Lwt.return_unit
       in
       Lwt.(
-        let clone_process_packet packet socket =
-          (* Copy the packet to the user's packets_recv_stream, then process it. *)
-          push_packet_recv (Some packet); process_packet packet socket
-        in
         let open_conn () =
           open' uri >>= fun (socket, packets) ->
-          Lwt_io.printl "waiting on my_stream (open_conn)..." >>= fun () ->
-          Lwt_stream.fold_s clone_process_packet packets socket
+          Lwt_stream.fold_s
+            (fun packet socket ->
+               push_packet_recv (Some packet);
+               process_packet socket packet)
+            packets socket
+            (* TODO: check we got an sid in the first set of packets. *)
+            (* TODO: we are processing these packets twice. *)
+        in
+        let rec poll_forever socket =
+          Lwt_io.printl "polling..." >>= fun () ->
+          Transport.Polling.do_poll socket.transport >>= fun packets ->
+          Lwt_stream.iter (fun packet -> push_packet_recv (Some packet)) packets >>= fun () ->
+          poll_forever socket
         in
         let rec maintain_connection socket  =
-          Lwt_unix.sleep 3.0 >>= fun () ->
-          write socket
-            ((Packet.PING, Packet.P_None)
-             :: Lwt_stream.get_available packets_send_stream)
-          >>= fun () ->
-          Transport.Polling.do_poll socket.transport >>= fun packets ->
-          Lwt_stream.fold_s clone_process_packet packets socket >>= fun socket ->
-          maintain_connection socket
+          match socket.handshake with
+          | None -> Lwt.fail_with "maintain_connection: socket not open"
+          | Some handshake ->
+            (
+             let sleep_until_ping () =
+               let return_ping () =
+                 push_packet_send (Some (Packet.PING, Packet.P_None));
+                 Lwt.return
+                   { socket with
+                     ping_sent_at = Some (Unix.time ())
+                   }
+               in
+               match socket.ping_sent_at with
+               | None ->
+                 Lwt_io.printl "no ping_sent_at: send ping now" >>= fun () ->
+                 return_ping ()
+               | Some ping_sent_at ->
+                 let timeout_on_pong () =
+                   let seconds_since_last_ping = Unix.time () -. ping_sent_at in
+                   let ping_timeout_seconds = (float_of_int handshake.ping_timeout) /. 1000.0 in
+                   Lwt_unix.timeout (ping_timeout_seconds -. seconds_since_last_ping)
+                 in
+                 (match socket.pong_received_at with
+                  | None ->
+                    (* We are waiting for PONG from server *)
+                    timeout_on_pong ()
+                  | Some pong_received_at when ping_sent_at > pong_received_at ->
+                    (* We are waiting for PONG from server *)
+                    timeout_on_pong ()
+                  | Some pong_received_at ->
+                    (* All good, send a PING at the next interval. *)
+                    let seconds_since_last_pong = Unix.time () -. pong_received_at in
+                    let ping_interval_seconds = (float_of_int handshake.ping_interval) /. 1000.0 in
+                    Lwt_unix.sleep (ping_interval_seconds -. seconds_since_last_pong) >>= fun () ->
+                    Lwt_io.printl "sleep_until_ping" >>= fun () ->
+                    return_ping ()
+                 )
+             in
+             let sleep_until_packet_to_send () =
+               Lwt_stream.peek packets_send_stream >>= fun _ ->
+               Lwt_io.printl "sleep_until_packet_to_send" >>= fun () ->
+               return socket
+             in
+             let sleep_until_packet_received () =
+               Lwt_stream.peek packets_recv_stream >>= fun _ ->
+               Lwt_io.printl "sleep_until_packet_received" >>= fun () ->
+               return socket
+             in
+             pick
+               (* TODO: how to ensure none of these are dropped if >1 returns at once? *)
+               [ sleep_until_ping ()
+               ; sleep_until_packet_to_send ()
+               ; sleep_until_packet_received ()
+               ]
+             >>= fun socket ->
+             (* TODO: how do we propagate updates to the socket state to the poll_forever thread? *)
+             Lwt_list.fold_left_s process_packet socket (Lwt_stream.get_available packets_recv_stream) >>= fun socket ->
+             write socket (Lwt_stream.get_available packets_send_stream) >>= fun () ->
+             return socket)
+            >>= maintain_connection
         in
         open_conn () >>= fun socket ->
         pick
-          [ maintain_connection socket
-          ; f recv send
+          [ poll_forever socket
+          ; maintain_connection socket
+          ; f (Lwt_stream.clone packets_recv_stream) send
           ]
       )
 end
@@ -471,20 +552,23 @@ let main () =
           ()
       in
       Socket.with_connection uri
-        (fun recv send ->
-           let rec loop i () =
-             if i > 2 then
-               return_unit
-             else
-               recv () >>= (function
-                   | (Packet.PONG, _) -> Lwt_io.printl "PONG!"
-                   | (packet_type, _) ->
-                     Lwt_io.printlf "user got a packet '%s', responding with a message."
-                       (Packet.string_of_packet_type packet_type)
-                     >>= fun () ->
-                     send (Packet.MESSAGE, Packet.P_String "hello there")
-                 )
-               >>= fun () ->
-               loop (i + 1) ()
-           in loop 0 ())
+        (fun packets send_packet ->
+           let react = function
+             | Some (packet_type, _) ->
+               Lwt_io.printlf "-- User got a packet '%s'!"
+                 (Packet.string_of_packet_type packet_type)
+             | None ->
+               Lwt_io.printl "End of packet stream?" >>= fun () ->
+               Lwt.fail Exit
+           in
+           let rec react_forever () = Lwt_stream.get packets >>= react >>= react_forever
+           in
+           let rec sendline () =
+             Lwt_io.(read_line_opt stdin) >>= function
+             | None ->
+               Lwt.return_unit
+             | Some content ->
+               send_packet (Packet.MESSAGE, Packet.P_String content) >>= sendline
+           in
+           sendline () <?> react_forever ())
     )
