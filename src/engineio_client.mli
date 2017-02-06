e3d5b5e110220c363260d8745c4d49971171b674
Matt Bray <mattjbray@gmail.com>
2017-02-06 11:09:44 +0000

    Move test program.

--- src/engineio_client.mli
+++ src/engineio_client.mli
@@ -1,1 +1,26 @@
-val main : unit -> unit
+module Packet : sig
+  type packet_type =
+    | OPEN
+    | CLOSE
+    | PING
+    | PONG
+    | MESSAGE
+    | UPGRADE
+    | NOOP
+    | ERROR
+
+  type packet_data =
+    | P_None
+    | P_String of string
+    | P_Binary of int list
+
+  type t = packet_type * packet_data
+
+  val string_of_packet_type : packet_type -> string
+  val packet_type_of_int : int -> packet_type
+  val int_of_packet_type : packet_type -> int
+end
+
+module Socket : sig
+  val with_connection :  Uri.t -> ((Packet.t Lwt_stream.t) -> (string -> unit Lwt.t) -> 'a Lwt.t) -> 'a Lwt.t
+end
--- test/main.ml
+++ test/main.ml
@@ -1,2 +1,35 @@
 let () =
-  Engineio_client.main ()
+  Lwt_main.run
+    Lwt.(Engineio_client.(
+        Lwt_io.printl "running..." >>= fun () ->
+        let uri =
+          Uri.make
+            ~scheme:"http"
+            ~host:"localhost"
+            (* ~port:3000 *)
+            (* ~path:"socket.io/" *)
+            ~port:3001
+            ~path:"engine.io/"
+            ()
+        in
+        Socket.with_connection uri
+          (fun packets send ->
+             let react = function
+               | Some (packet_type, _) ->
+                 Lwt_io.printlf "-- User got a packet %s!"
+                   (Packet.string_of_packet_type packet_type |> String.uppercase_ascii)
+               | None ->
+                 Lwt_io.printl "End of packet stream?" >>= fun () ->
+                 Lwt.fail Exit
+             in
+             let rec react_forever () =
+               Lwt_stream.get packets >>= react >>= react_forever
+             in
+             let rec sendline () =
+               Lwt_io.print "> " >>= fun () ->
+               Lwt_io.(read_line_opt stdin) >>= function
+               | None -> Lwt_io.printl "Good-bye!"
+               | Some content -> send content >>= sendline
+             in
+             sendline () <?> react_forever ())
+      ))
